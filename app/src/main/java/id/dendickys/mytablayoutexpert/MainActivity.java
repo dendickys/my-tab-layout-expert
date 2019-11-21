f851b4b9ec44f5d26adfd623cc97ff42c3ecfc1a
dendickys <febri.unquad2014@gmail.com>
2019-11-21 07:24:18 +0700

    tablayout exercise finished

--- app/src/main/java/id/dendickys/mytablayoutexpert/MainActivity.java
+++ app/src/main/java/id/dendickys/mytablayoutexpert/MainActivity.java
@@ -1,8 +1,13 @@
 package id.dendickys.mytablayoutexpert;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
     }
 }
